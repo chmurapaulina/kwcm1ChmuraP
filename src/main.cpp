82f178b51756a0e0c0439d467220d36ad858c43c
chmurapaulina <paulchm347@student.polsl.pl>
2019-03-20 14:04:16 +0100

    Trzeci - opcja wlaczona

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
-#include "trygonometria.h"
-#include<iostream>
 
-using namespace degreemath;
+#include<iostream>
 using namespace std;
 
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
+using namespace degreemath;
+#else
+#define _USE_MATH_DEFINES
+#include "math.h"
+#include<cmath>
+#endif
+
 int main(){
+#ifdef USE_TRIGONOMETRY_DEGREE
+	str::cout<<"STOPNIE"<<std::endl;
 	std::cout<<"Sin 0: " << degreemath::sin(0)<<std::endl;
 	std::cout<<"Sin 30: " << degreemath::sin(30)<<std::endl;
 
@@ -16,6 +25,15 @@ int main(){
 
 	std::cout<<"Ctg 0: " << degreemath::ctg(0)<<std::endl;
 	std::cout<<"Ctg 30: " << degreemath::ctg(30)<<std::endl;
+#else
+	std::cout<<"RADIANY"<<std::endl;
+	std::cout<<"Sin 30: " << std::sin(M_PI/6)<<std::endl;
 
+	std::cout<<"Cos 30: " << std::cos(M_PI/6)<<std::endl;
+
+	std::cout<<"Tg 30: " << std::tan(M_PI/6)<<std::endl;
+
+	std::cout<<"Ctg 30: " << 1/std::tan(M_PI/6)<<std::endl;
+#endif
 return 0;
 }
